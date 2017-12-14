7f8b0d3a0d53a28d8da9a51fd4ec51335c8c77c9
Sylvain <sylvain.maillot1@gmail.com>
2017-12-14 12:17:39 +0100

    Adaptation du wiki pour la page API.html

--- wiki.js
+++ wiki.js
@@ -1,8 +1,9 @@
-var artist = "muse";
-var result;
-
-function request(art) {
-	fetch("https://fr.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=extracts&exintro=&explaintext=&titles="+ art, {
+function request() {
+	var artist = document.getElementById("query").value;
+	q2=capitalizeFirstLetter(artist);
+	console.log(q2);
+	var result;
+	fetch("https://fr.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=extracts&exintro=&explaintext=&titles="+ q2, {
 	    method: 'POST',
 	    headers: new Headers( {'Api-User-Agent': 'Example/1.0'})
 	        } ).
@@ -15,19 +16,25 @@ function request(art) {
 	    for (k in t);
 	    	id = k;
 	    	result = t[id].extract;
-	    	console.log(result);
-	    	console.log(art);
 	    	if (result == "" && !art.includes("_(groupe)")) 
 	    		return request(art+"_(groupe)");
-	    	test(result);
+	    	add(result,artist);
 	});
 }
 
-function test(param) {
+function add(param,artist) {
 	if (param == "") 
 		document.getElementById("wikimedia").innerHTML = "L'artiste demandé n'a pas été trouvé sur wikipedia";
 	else
-		document.getElementById("wikimedia").innerHTML = param;
+		document.getElementById("wikimedia").innerHTML = "<center><b>"+artist+"</b></center><br>"+param;
 }
 
-request(artist);
+function capitalizeFirstLetter(string) {
+	string = string.charAt(0).toUpperCase() + string.slice(1);
+	var ind = string.indexOf(' ');
+	while(ind != -1) {
+    	string = string.substr(0, ind)+"_"+string.charAt(ind+1).toUpperCase() + string.substr(ind+2,string.length);
+    	ind = string.indexOf(' ');
+	}
+	return string;
+}
